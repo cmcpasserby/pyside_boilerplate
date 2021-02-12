b49f38b3d944460740c037c44508f94ade69bb81
Chris Cunningham <chris@rightsomegoodgames.ca>
2021-02-11 23:41:49 -0400

    added basic windows support for creating envirments and pyinstaller

---
+++ make.ps1
@@ -0,0 +1,26 @@
+$cmd=$args[0]
+
+switch ($cmd) {
+    clean {
+        Remove-Item build -Recurse -ErrorAction Ignore
+        Remove-Item dist -Recurse -ErrorAction Ignore
+        Remove-Item venv\pyinstaller -Recurse -ErrorAction Ignore
+    }
+
+    devenv {
+        virtualenv --clear --python=python3.9.1 venv/dev
+        venv\dev\Scripts\activate.ps1
+        pip install -r requirements\app.txt
+        deactivate
+        Write-Host "dev virtualenv made at venv/dev ('source venv/dev/bin/activate' to activate)"
+    }
+
+    pyinstaller {
+        virtualenv --clear --python=python3.9.1 venv\pyinstaller
+        venv\pyinstaller\Scripts\activate.ps1
+        pip install -r requirements\pyinstaller.txt
+        pip install -r requirements\app.txt
+        pyinstaller -y misc\app.spec
+        deactivate
+    }
+} 
